15d19a37cfa5215204e046fb9d9c2522a16a6aad
Blessed <codingdreams4ever@gmail.com>
2019-04-10 00:46:26 -0500

    completed all TODOs

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -6,13 +6,10 @@ import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by LaunchCode
@@ -25,23 +22,12 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "/job/{id}", method = RequestMethod.GET)
-    public String index(Model model, @PathVariable int id) {
-
-        // TODO #1 - get the Job with the given ID and pass it into the view
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
 
         Job someJob = jobData.findById(id);
         model.addAttribute("jobs", someJob);
-        Employer employers = jobData.getEmployers().findById(id);
-        model.addAttribute("employers", employers);
-        Location locations = jobData.getLocations().findById(id);
-        model.addAttribute("locations", locations);
-        CoreCompetency coreCompetencies = jobData.getCoreCompetencies().findById(id);
-        model.addAttribute("coreCompetencies", coreCompetencies);
-        PositionType positionTypes = jobData.getPositionTypes().findById(id);
-        model.addAttribute("positionTypes", positionTypes);
         return "job-detail";
-
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
@@ -51,18 +37,23 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(@Valid JobForm jobForm, Errors errors) {
 
         if (errors.hasErrors()) {
             return "new-job";
         }
         if (!errors.hasErrors()) {
-            Job newJob = new Job();
+
+            Job newJob = new Job(jobForm.getName(),
+                    jobData.getEmployers().findById(jobForm.getEmployerId()),
+                    jobData.getLocations().findById(jobForm.getLocationId()),
+                    jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+                    jobData.getCoreCompetencies().findById(jobForm.getCoreCompetenciesId()));
             jobData.add(newJob);
+
+            return "redirect:?id=" + newJob.getId();
+
         }
-        return "redirect:";
+        return "redirect:?id=";
     }
 }
-// TODO #6 - Validate the JobForm model, and if valid, create a
-    // new Job and add it to the jobData data store. Then
-    // redirect to the job detail view for the new Job.
